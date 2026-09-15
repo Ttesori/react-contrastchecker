283e9b97a7a34e95b976a07ea2a1f2367849046d
Toni Tesori <tt@tonitesori.dev>
2026-09-15 16:36:19 -0600

    Move computeContrastRatio to utils

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -3,3 +3,7 @@ import Color from "colorjs.io";
 export function isSameColor(a: Color, b: Color): boolean {
   return a.toString({ format: "hex" }) === b.toString({ format: "hex" });
 }
+
+export function computeContrastRatio(colorA: Color, colorB: Color): number {
+  return colorA.contrast(colorB, "WCAG21");
+}
